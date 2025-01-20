5795e049212e8e4aa8c265bf77e68ae0cfce84bd
Preetam Bhosle <preetb123@gmail.com>
2025-01-20 11:58:09 +0530

    added pages workflow

--- site/src/jsMain/kotlin/dev/preetb123/pages/Index.kt
+++ site/src/jsMain/kotlin/dev/preetb123/pages/Index.kt
@@ -2,13 +2,20 @@ package dev.preetb123.pages
 
 import androidx.compose.runtime.*
 import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.padding
+import com.varabyte.kobweb.compose.ui.modifiers.width
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.text.SpanText
 import org.jetbrains.compose.web.dom.Text
 import com.varabyte.kobweb.worker.rememberWorker
 import dev.preetb123.worker.EchoWorker
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.Br
 
 @Page
 @Composable
@@ -20,6 +27,11 @@ fun HomePage() {
 
     // TODO: Replace the following with your own content
     Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Hello \uD83D\uDC4B")
+        Column(modifier = Modifier.maxWidth(1024.px).padding(24.px)) {
+            SpanText("Hello, I am a Software Engineer specializing in cross-platform apps development primarily with Compose/Kotlin Multiplatform.")
+            SpanText("I like to do high impact work with minimal efforts. Constantly in quest for technologies that simplify dev and end user experience.")
+            Br()
+            SpanText("Contact me: preetb123 at gmail.com")
+        }
     }
 }
